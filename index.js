c13be1063e86c89a1b8bc8bc3572d96e8e952453
AlexandrShab <93769197+AlexandrShab@users.noreply.github.com>
2024-04-27 09:00:34 +0300

    err fixed

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/api', async (req, res) => {
     console.log(req.query)
     let result = { ok: false, result: "no URL specified" }
     if (req.query && req.query.hasOwnProperty('url')) {
-        result = await fetchUrl(req.query.url, { method: get, headers: { Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOWZkYTRjMWQ4YjA3ODAyYjM2MjMxYTI1ZTAwOTBlZiIsInN1YiI6IjY2MmFhYmJlNTAxY2YyMDExZGIzM2I5MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pHEhpbpJq7-FfLHmR2C1-Y1E_8qn1h2QL2INlmKPnyw' } })
+        result = await fetchUrl(req.query.url, { method: 'get', headers: { Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOWZkYTRjMWQ4YjA3ODAyYjM2MjMxYTI1ZTAwOTBlZiIsInN1YiI6IjY2MmFhYmJlNTAxY2YyMDExZGIzM2I5MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pHEhpbpJq7-FfLHmR2C1-Y1E_8qn1h2QL2INlmKPnyw' } })
     }
     res.send(result)
 
