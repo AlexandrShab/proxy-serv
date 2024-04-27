2e687562c5df8f928375a72a75a03235c883c76c
AlexandrShab <93769197+AlexandrShab@users.noreply.github.com>
2024-04-27 08:38:44 +0300

    AAA

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ app.get('/', async (req, res) => {
     console.log(req.query)
     let result = await fetchUrl(req.query.url)
     res.send(result)
+
+})
+
+app.get('/api', async (req, res) => {
+    console.log(req)
+    let result = await fetchUrl(req.query.url, { method: get, headers: { Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOWZkYTRjMWQ4YjA3ODAyYjM2MjMxYTI1ZTAwOTBlZiIsInN1YiI6IjY2MmFhYmJlNTAxY2YyMDExZGIzM2I5MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pHEhpbpJq7-FfLHmR2C1-Y1E_8qn1h2QL2INlmKPnyw' } })
+    res.send(result)
+
 })
 
 app.post('/', async (req, res) => {
